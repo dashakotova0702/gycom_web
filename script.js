cb8d49a0c826338b78c204cc41860298f777afd7
dashakotova0702 <dashakotova0702@gmail.com>
2023-08-22 15:15:09 +0300

    index done

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(window).scroll(function() {
         $(".dropdown_menu").css("background-color", "white");
         $(".container_menu").css("background-color", "white");
         $(".container_menu").css("box-shadow", "0 0.4em 5px rgba(122,122,122,0.2)");
+        $(".dropdown").css("box-shadow", "0 0.5em 15px rgba(122,122,122,0.5)");
     } else {
         $(".text_menu").css("color", "white");
         $(".home").css("display", "block");
@@ -13,5 +14,26 @@ $(window).scroll(function() {
         $(".dropdown_menu").css("background-color", "transparent");
         $(".container_menu").css("background-color", "transparent");
         $(".container_menu").css("box-shadow", "none");
+        $(".dropdown").css("box-shadow", "none");
     }
+});
+$(document).ready(function(){
+    $('.slider_products').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        infinite: true,
+        arrows: false
+    });
+    $('.slider_news').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        infinite: true,
+        arrows: false
+    });
 });
